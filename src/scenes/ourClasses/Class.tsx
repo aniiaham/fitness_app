772fb92fef6f51d3960173c86bc7e29256134731
aniiaham <aniya_axm@icloud.com>
2023-03-14 21:06:07 -0400

    classes page

---
+++ src/scenes/ourClasses/Class.tsx
@@ -0,0 +1,23 @@
+type Props = {
+  name: string;
+  description?: string;
+  image: string;
+};
+
+const Class = ({ name, description, image }: Props) => {
+
+    const overlayStyles = `p-5 absolute z-30 flex 
+    h-[380px] w-[450px] flex-col items-center justify-center
+    whitespace-normal bg-primary-500 text-center text-white
+    opacity-0 transition duration-500 hover:opacity-90`;
+
+  return <li className="relative mx-5 in-line-block h-[380px] w-[450px]">
+    <div className={overlayStyles}>
+        <p>{name}</p>
+        <p>{description}</p>
+        <img alt={`${image}`} src={image} />
+    </div>
+  </li>;
+};
+
+export default Class;
